858e02c8b503c0b01bf7953a65bceaa610d1ce4c
Boolean <hongbin.hsu@bincent.com>
2019-04-05 20:09:34 +0800

    no message

--- consul.go
+++ consul.go
@@ -1,53 +1,114 @@
 package gutil
 
 import (
-	"fmt"
-	"github.com/bincent/gutil"
+	"errors"
+	"github.com/bincent/balance"
 	"github.com/hashicorp/consul/api"
 	"time"
 )
 
-// ConsulRegister consul service register
+type Consul struct {
+	Client 	*api.Client
+}
+
+type ConsulDiscover struct {
+	ServiceName                    string
+	Balance                        string
+}
+
 type ConsulRegister struct {
-	Address                        string
-	Service                        string
-	Tag                            []string
+	ServiceID					   string
+	ServiceName                    string
+	ServiceTag                     []string
 	Port                           int
-	CheckUri					   string
+	MonitorAddress				   string
 	Timeout                        time.Duration
 	Interval                       time.Duration
 	DeregisterCriticalServiceAfter time.Duration
 }
 
-// Register register service
-func (this *ConsulRegister) Register() error {
-	config := api.DefaultConfig()
-	config.Address = this.Address
-	client, err := api.NewClient(config)
-	if err != nil {
-		return err
+// 注册服务
+func (this *Consul) Register(register *ConsulRegister) error {
+	ip := LocalIP()
+
+	if len(register.ServiceName) > 0 {
+		return errors.New("must need Service Name")
 	}
-	agent := client.Agent()
 
-	IP := gutil.LocalIP()
+	//if (register.Port == nil) {
+	//	return errors.New("must need Service Port")
+	//}
 
-	reg := &api.AgentServiceRegistration{
-		ID:      fmt.Sprintf("%v-%v-%v", this.Service, IP, this.Port), // 服务节点的名称
-		Name:    fmt.Sprintf("grpc.health.v1.%v", this.Service),    // 服务名称
-		Tags:    this.Tag,                                          // tag，可以为空
-		Port:    this.Port,                                         // 服务端口
-		Address: IP,                                             // 服务 IP
-		Check: &api.AgentServiceCheck{ // 健康检查
-			HTTP:                           this.CheckUri,								  // 健康检测地址
-			Timeout:                        this.Timeout.String(),						  // 超时
-			Interval: 						this.Interval.String(),                       // 健康检查间隔
-			DeregisterCriticalServiceAfter: this.DeregisterCriticalServiceAfter.String(), // 注销时间，相当于过期时间
+	if len(register.ServiceID) > 0 {
+		register.ServiceID = register.ServiceName + "-" + ip
+	}
+
+	if len(register.MonitorAddress) > 0 {
+		register.MonitorAddress = "tcp://" + ip
+	}
+
+	if (register.Timeout.String() == "") {
+		register.Timeout = 3
+	}
+
+	if (register.Interval.String() == "") {
+		register.Interval = 5
+	}
+
+	if (register.DeregisterCriticalServiceAfter.String() == "") {
+		register.DeregisterCriticalServiceAfter = 30
+	}
+
+	service := &api.AgentServiceRegistration{
+		ID:      register.ServiceID,
+		Name:    register.ServiceName,
+		Port:    register.Port,
+		Address: ip,
+		Tags:    register.ServiceTag,
+		Check: &api.AgentServiceCheck{
+			TCP:     register.MonitorAddress,
+			Interval: string(register.Interval * time.Second),
+			Timeout:  string(register.Timeout * time.Second),
+			DeregisterCriticalServiceAfter: string(register.DeregisterCriticalServiceAfter * time.Second),
 		},
 	}
 
-	if err := agent.ServiceRegister(reg); err != nil {
+	if err := this.Client.Agent().ServiceRegister(service); err != nil {
 		return err
 	}
 
 	return nil
+}
+
+// 服务发现
+func (this *Consul) Discover(discover *ConsulDiscover) (address string, err error) {
+	var insts []*balance.Instance
+
+	services, _, err := this.Client.Catalog().Services(&api.QueryOptions{})
+	for name := range services {
+		servicesData, _, err := this.Client.Health().Service(name, "", true,
+			&api.QueryOptions{})
+		if err != nil { return "", err }
+
+		for _, entry := range servicesData {
+			if discover.ServiceName != entry.Service.Service {
+				continue
+			}
+
+			for _, health := range entry.Checks {
+				if health.ServiceName != discover.ServiceName {
+					continue
+				}
+
+				node := balance.NewInstance(entry.Service.Address, entry.Service.Port)
+				insts = append(insts, node)
+			}
+		}
+	}
+
+	return balance.Round(insts)
+}
+
+func (this *Consul) SetClient(client *api.Client) {
+	this.Client = client
 }
